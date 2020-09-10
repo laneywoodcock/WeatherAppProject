703c01b45a30283d67c26edadb7ef82ac3a82381
laneywoodcock <67925893+laneywoodcock@users.noreply.github.com>
2020-09-10 14:55:47 -0500

    Added 12 hour forecast projection

--- src/script.js
+++ src/script.js
@@ -61,6 +61,7 @@ function handlePosition(position) {
 
 //Initial h1 Display (local weather)
 function showWeather(response) {
+  
     let h1 = document.querySelector("#display-temperature");
     let h2 = document.querySelector("#degrees");
     let temperature = Math.round(response.data.main.temp);
@@ -75,6 +76,9 @@ function showWeather(response) {
     humidity.innerHTML = `${response.data.main.humidity}% Humidity`;
     wind.innerHTML = Math.round(response.data.wind.speed);
     icon.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    
+    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${response.data.name}&appid=${apiKey}&&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 //Submit Button
@@ -83,12 +87,56 @@ function searchWeather(event) {
   let input = document.querySelector("#city-input");
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&&units=imperial`;
   axios.get(apiUrl).then(showWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${input.value}&appid=${apiKey}&&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
 } 
 
 let form = document.querySelector("form");
 form.addEventListener("submit", searchWeather);
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let forecast = null; 
+  forecastElement.innerHTML = null;
+ 
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+    <div class="col-2">
+      <h3> 
+      ${formatHours(forecast.dt * 1000)} 
+      </h3>
+      <img 
+        src="https://openweathermap.org/img/wn/${
+          forecast.weather[0].icon
+              }@2x.png" 
+        id="icon"
+        width=50px
+        >
+      <div class="weather-forecast-temperature">
+       <strong>
+      ${Math.round(forecast.main.temp_max)}°
+       </strong>
+       / ${Math.round(forecast.main.temp_min)}°
+     </div>   
+    </div>
+`;
+  }
+}
 
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
 
 
 function displayFahrenheitTemperature(event) {
